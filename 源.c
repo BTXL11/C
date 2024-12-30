cb3fd447d0a15b50ecb50050d4a63096204270f2
BTXL11 <B24090221@njupt.edu.cn>
2024-12-30 21:24:24 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,14 +1,35 @@
 #include<stdio.h>
-#include<math.h>
-#include"≤‚ ‘.h"
+#include"hhh.cpp"
 int main()
 {
-	printf("fib(10)=%d\n", fib(10));
+	struct student
+	{
+		char name[20];
+		int id;
+		int englishscore;
+		int mathscore;
+		int physicsscore;
+
+	};
+	struct student s[10];
+	int n;
+	scanf_s("%d", &n);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		scanf_s("%d %s %d %d %d",&s[i].id, s[i].name, &s[i].mathscore, &s[i].physicsscore, &s[i].englishscore);
+	}
+	int average[10];
+	for (i = 0; i < n; i++)
+	{
+		average[i] = (s[i].englishscore + s[i].mathscore + s[i].physicsscore) / 3;
+	}
+	for (i = 0; i < n; i++)
+		printf("The average score of the %dth student is %d.\n", i + 1, average[i]);
+
+
 	return 0;
 }
 
-
-
-   
 
 
